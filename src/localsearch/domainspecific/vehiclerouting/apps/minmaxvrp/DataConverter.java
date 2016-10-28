ae911e029a2b1d5333891ea68e5b43f1b7734951
dungkhmt <dungkhmt@gmail.com>
2016-10-28 11:22:01 +0700

    [dungkhmt][cblsvr]

--- src/localsearch/domainspecific/vehiclerouting/apps/minmaxvrp/DataConverter.java
+++ src/localsearch/domainspecific/vehiclerouting/apps/minmaxvrp/DataConverter.java
@@ -5,7 +5,7 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class DataConverter {
-	public void standardizeData(){
+	public void standardizeDataChristophides(){
 		String[] fe = new String[]{
 				"E-n101-k14.vrp",
 				"E-n23-k3.vrp",
@@ -145,11 +145,124 @@ public class DataConverter {
 		}
 	}
 
+	public void convertKelly(String fni, String fno, int nbVehicles){
+		try{
+			Scanner in = new Scanner(new File(fni));
+			PrintWriter out = new PrintWriter(fno);
+			
+			int nbClients = in.nextInt();
+			int depot = nbClients + 1;
+			int capacity = in.nextInt();
+			int tmp1 = in.nextInt();
+			int tmp2 = in.nextInt();
+			double tmp3 = in.nextDouble();
+			double x_depot = in.nextDouble();
+			double y_depot = in.nextDouble();
+			int demand_depot = in.nextInt();
+			int nbPoints = nbClients + 1;
+			
+			int[] demand = new int[nbPoints+1];
+			double[] x = new double[nbPoints+1];
+			double[] y = new double[nbPoints+1];
 
+			for(int i = 1; i <= nbClients; i++){
+				x[i] = in.nextDouble();
+				y[i] = in.nextDouble();
+				System.out.println(i + ", fni =  " + fni + "\t  " + x[i] + " " + y[i]);
+				demand[i] = in.nextInt();
+			}
+			
+			// depot
+			x[depot] = x_depot;
+			y[depot] = y_depot;
+			demand[depot] = demand_depot;
+			
+			in.close();
+			
+			double[][] c = new double[nbPoints+1][nbPoints+1];
+			for(int i = 1; i <= nbPoints; i++){
+				for(int j = 1; j <= nbPoints; j++){
+					c[i][j] = Math.sqrt((x[i]-x[j])*(x[i]-x[j]) + (y[i]-y[j])*(y[i]-y[j]));
+				}
+			}
+			
+			// write to file
+			out.println(nbVehicles + " " + nbPoints);
+			out.println(depot + " " + capacity);
+			for(int i = 1; i <= nbPoints; i++)
+				out.println(i + " " + demand[i]);
+			for(int i = 1; i <= nbPoints; i++)
+				for(int j = 1; j <= nbPoints; j++)
+					out.println(i + " " + j + " " + c[i][j]);
+			out.close();
+			
+		}catch(Exception ex){
+			ex.printStackTrace();
+			System.exit(-1);
+		}
+	}
+	public void standardizeDataKelly(){
+		String[] fn = {
+			"kelly01.txt",
+			"kelly02.txt",
+			"kelly03.txt",
+			"kelly04.txt",
+			"kelly05.txt",
+			"kelly06.txt",
+			"kelly07.txt",
+			"kelly08.txt",
+			"kelly09.txt",
+			"kelly10.txt",
+			"kelly11.txt",
+			"kelly12.txt",
+			"kelly13.txt",
+			"kelly14.txt",
+			"kelly15.txt",
+			"kelly16.txt",
+			"kelly17.txt",
+			"kelly18.txt",
+			"kelly19.txt",
+			"kelly20.txt",
+		};
+		
+		int[] nbVehicles = new int[]{
+			10,
+			10,
+			10,
+			12,
+			5,
+			8,
+			9,
+			11,
+			14,
+			16,
+			18,
+			19,
+			27,
+			30,
+			34,
+			38,
+			22,
+			28,
+			33,
+			41,
+		};
+		
+		String dir = "data/MinMaxVRP/Kelly/";
+		
+		for(int i = 0; i < fn.length; i++){
+			String fni = dir + "/original/" + fn[i];
+			String fno = dir + "/std_all/" + fn[i];
+			convertKelly(fni, fno, nbVehicles[i]);
+		}
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		DataConverter DC = new DataConverter();
-		DC.standardizeData();
+		//DC.standardizeDataChristophides();
+		DC.standardizeDataKelly();
 	}
 
 }
