0f091c2e2f29aff9b333c8faa10dec1946100698
dungkhmt <dungkhmt@gmail.com>
2017-05-07 10:11:06 +0700

    dungkhmt

--- src/localsearch/domainspecific/vehiclerouting/vrp/VarRoutesVR.java
+++ src/localsearch/domainspecific/vehiclerouting/vrp/VarRoutesVR.java
@@ -1757,7 +1757,9 @@ public class VarRoutesVR{
     	}
     	HashSet<Point> set = new HashSet<Point>(y);
     	for (Point p : x) {
-    		if ((!isClientPoint(p)) || set.contains(p)) {
+    		if ((!isClientPoint(p)) 
+    				//|| set.contains(p)
+    				) {
     			return false;
     		}
     	}
@@ -1767,7 +1769,7 @@ public class VarRoutesVR{
     		int ix = getIndex(px);
     		int iy = getIndex(py);
     		if(ix == Constants.NULL_POINT && iy == Constants.NULL_POINT) return false;
-    		if(next(py) == px) return false;
+    		//if(next(py) == px) return false;
     		
     		//if(route[ix] == Constants.NULL_POINT && route[iy] == Constants.NULL_POINT) return false;
     	}
